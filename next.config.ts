5e9ff33f74db636a78b06f3756554c2e221cbb0e
James Brace <48393754+jbrace817@users.noreply.github.com>
2026-01-16 14:20:43 -0500

    Add image configuration to next.config.ts for optimized caching and remote image loading from CloudFront.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    minimumCacheTTL: 31536000,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "deifkwefumgah.cloudfront.net",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
